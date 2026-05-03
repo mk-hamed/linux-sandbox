762e8a0f478091cbfb9fb1b9b87b1a7ccb6b9789
mk-hamed <klaibmo@gmail.com>
2026-05-02 22:58:24 -0500

    fix: typo in kubernetes provider name

--- terraform/0-terraform.tf
+++ terraform/0-terraform.tf
@@ -21,7 +21,7 @@ provider "aws" {
   }
 }
 
-provider "kubernets" {
+provider "kubernetes" {
   host = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
