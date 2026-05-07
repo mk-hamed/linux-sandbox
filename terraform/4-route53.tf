620496a986912cb0ec838b9818b0c2b7a3e2dbff
mk-hamed <klaibmo@gmail.com>
2026-05-07 18:50:36 -0500

    feat: updated route53 config to link to kubernetes config

--- terraform/4-route53.tf
+++ terraform/4-route53.tf
@@ -1,20 +1,15 @@
-data "kubernetes_service" "linux_sandbox" {
-  metadata {
-    name      = "linux-sandbox"
-    namespace = "linux-sandbox"
-  }
-}
-
 resource "aws_route53_record" "linux_sandbox" {
   zone_id = data.aws_route53_zone.linux_sandbox.zone_id
   name    = "linuxsandbox.dev"
   type    = "A"
 
   alias {
-    name                   = data.kubernetes_service.linux_sandbox.status[0].load_balancer[0].ingress[0].hostname
+    name                   = kubernetes_service_v1.linux_sandbox.status[0].load_balancer[0].ingress[0].hostname
     zone_id                = var.elb_zone_id
     evaluate_target_health = true
   }
+
+  depends_on = [kubernetes_service_v1.linux_sandbox]
 }
 
 data "aws_route53_zone" "linux_sandbox" {
