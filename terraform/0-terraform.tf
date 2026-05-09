6d167bec0f1c9642bf0d1fdec6d29b342a992b5a
mk-hamed <klaibmo@gmail.com>
2026-05-09 00:01:43 -0500

    feat: add helm pill in html, add helm provider

--- terraform/0-terraform.tf
+++ terraform/0-terraform.tf
@@ -50,3 +50,12 @@ provider "helm" {
     }
   }
 }
+
+resource "helm_release" "ingress-nginx" {
+  name             = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  version          = ""
+  chart            = "chart"
+  create_namespace = true
+}
