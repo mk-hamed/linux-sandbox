85f80a28076eb0d8a6f0a7b2b2ff32a486a716b1
mk-hamed <klaibmo@gmail.com>
2026-05-10 17:39:27 -0500

    feat: configure landing page manifest and routing

--- terraform/5-kubernetes.tf
+++ terraform/5-kubernetes.tf
@@ -6,6 +6,10 @@ resource "kubernetes_namespace" "linux_sandbox" {
   depends_on = [module.eks]
 }
 
+################################
+#### ttyd terminal manifest ####
+################################
+
 resource "kubernetes_deployment_v1" "linux_sandbox" {
   metadata {
     name      = "linux-sandbox"
@@ -60,15 +64,74 @@ resource "kubernetes_service_v1" "linux_sandbox" {
     port {
       name        = "http"
       protocol    = "TCP"
-      port        = 80
+      port        = 7681
       target_port = 7681
     }
+  }
+
+  depends_on = [module.eks]
+}
+
+################################
+#### landing page manifest ####
+################################
+
+resource "kubernetes_deployment_v1" "linux_sandbox_landing" {
+  metadata {
+    name      = "linux-sandbox-landing"
+    namespace = kubernetes_namespace.linux_sandbox.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "linux-sandbox-landing"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "linux-sandbox-landing"
+        }
+      }
+
+      spec {
+        container {
+          name  = "linux-sandbox-landing"
+          image = "705738638798.dkr.ecr.us-east-1.amazonaws.com/linux-sandbox-landing:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "kubernetes_service_v1" "linux_sandbox_landing" {
+  metadata {
+    name      = "linux-sandbox-landing"
+    namespace = kubernetes_deployment_v1.linux_sandbox_landing.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "linux-sandbox-landing"
+    }
+
+    type = "ClusterIP"
 
     port {
-      name        = "https"
+      name        = "http"
       protocol    = "TCP"
-      port        = 443
-      target_port = 7681
+      port        = 80
+      target_port = 80
     }
   }
 
